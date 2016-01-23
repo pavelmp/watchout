857f67e324e950a1ee6b39e1836d343da19f1440
pavelmp <pavelmp@gmail.com>
2016-01-23 15:47:43 -0800

    Add image to svg with working movements

--- client/watchout.js
+++ client/watchout.js
@@ -1,10 +1,3 @@
-/*
-  .append("image")
-  .atter("xlink:href", "asteroid.png")
-  .attr("width", 32)
-  .attr("height", 32)
-*/
-
 var gameParameters = {
   height: 450,
   width: 700,
@@ -14,7 +7,23 @@ var gameParameters = {
 
 var gameStats = {
   score: 0,
-  bestScore: 0
+  bestScore: 0,
+  collisions: 0
+};
+
+var updateScore = function(){
+  d3.select('.current').select('span').text(gameStats.score.toString());
+};
+
+var updateBestScore = function(){
+  var maxScore = Math.max(gameStats.bestScore, gameStats.score);
+  gameStats.bestScore = maxScore;
+  d3.select('.highscore').select('span').text(maxScore.toString());
+};
+
+var increaseScore = function() {
+  gameStats.score++;
+  updateScore();
 };
 
 var limiter = function(axis, value, size) {
@@ -29,28 +38,18 @@ var limiter = function(axis, value, size) {
 };
 
 var gameBoard = d3.select('.board').append("svg")
-                                   .attr('width', 700)
-                                   .attr('height', 450);
+                                   .attr('width', gameParameters.width)
+                                   .attr('height', gameParameters.height);
 
-gameBoard.append("rect").attr('width', 700)
-                        .attr('height', 450)
+gameBoard.append("rect").attr('width', gameParameters.width)
+                        .attr('height', gameParameters.height)
                         .attr('fill','blue');
 
-var updateScore = function(){
-  d3.select('.current').select('span').text(gameStats.score.toString());
-};
-
-var updateBestScore = function(){
-  var maxScore = Math.max(gameStats.bestScore, gameStats.score);
-  gameStats.bestScore = maxScore;
-  d3.select('.highscore').select('span').text(maxScore.toString());
-};
 
 
 var drag = d3.behavior.drag().on('drag', function(){
   d3.select(this).attr("cx", limiter('x',d3.event.x,0))
                       .attr("cy", limiter('y',d3.event.y,0));
-                      // .attr("transform","rotate(" + angle + "deg)");
 });
 
 var player = gameBoard.append('circle')
@@ -66,80 +65,73 @@ var enemizer = function(){
   for(var i = 0 ; i < gameParameters.nEnemies; i++){
     var enemy = {
       id: i,
-      x: limiter('x', Math.random()*gameParameters.width,0),
-      y: limiter('y', Math.random()*gameParameters.height,0),
+      x: Math.floor(limiter('x', Math.random()*gameParameters.width,0)),
+      y: Math.floor(limiter('y', Math.random()*gameParameters.height,0)),
       r: 0
     };
     enemies.push(enemy);
   }
 
-  gameBoard.selectAll('circle.enemy')
+  var svg = gameBoard.selectAll('svg.enemy')
                      .data(enemies)
                      .enter()
-                     //.append("svg")
-                     .append("circle")
-                     .attr("cx", function(d){return d.x ;})
-                     .attr("cy", function(d){return d.y ;})
-                     .attr('r',0)
-                     //.attr("width", 32)
-                     //.attr("height", 32)
-                     // .attr("color","red")
-                     .attr("fill","red")
+                     .append("svg")
                      .attr('class','enemy')
-                     //.append("image")
-                     //.attr("xlink:href", "asteroid.png")
-                     //.style("left","150px")
-                     //.style("top","150px")
+                     .attr("width", 32)
+                     .attr("height", 32)
+                     .attr("x", function(d){return d.x;})
+                     .attr("y",function(d){return d.y;});
+
+  var g = svg.append("g");
+  var img = g.append("svg:image")
+                     .attr("xlink:href", "asteroid.png")
+                     .attr("width", 32)
+                     .attr("height", 32);           
 };
 
 var moveEnemies = function(){
   for(var i = 0; i < enemies.length; i++) {
-    enemies[i].x = limiter('x', Math.random()*gameParameters.width,0);
-    enemies[i].y = limiter('y', Math.random()*gameParameters.height,0);
+    enemies[i].x = Math.floor(limiter('x', Math.random()*gameParameters.width,0));
+    enemies[i].y = Math.floor(limiter('y', Math.random()*gameParameters.height,0));
   }
-  d3.selectAll('.enemy').data(enemies)
-                        .transition()
-                        .duration(500)
-                        .attr('r', 10)
+  d3.selectAll('svg.enemy').data(enemies)
                         .transition()
                         .duration(2000)
-                        .attr("cx", function(d){return d.x;})
-                        .attr("cy", function(d){return d.y;})
+                        .attr("x", function(d){return d.x;})
+                        .attr("y",function(d){return d.y;})
                         .tween('fancyTween', ourTween);
 };
-var increaseScore = function() {
-  gameStats.score++;
-  updateScore();
-};
+
 var onCollision = function() {
   updateBestScore();
   gameStats.score = 0;
   updateScore();
+  d3.select('.collisions').select('span').text(gameStats.collisions++);
 };
 
 var checkCollision = function(enemy){
-  var radiusSum =  parseFloat(enemy.attr('r')) + parseFloat(player.attr('r'));
-  var xDiff = parseFloat(enemy.attr('cx')) - player.attr('cx');
-  var yDiff = parseFloat(enemy.attr('cy')) - player.attr('cy');
+  var radiusSum =  parseFloat(enemy.attr('width')/2) + parseFloat(player.attr('r'));
+  var xDiff = parseFloat(enemy.attr('x')) - player.attr('cx');
+  var yDiff = parseFloat(enemy.attr('y')) - player.attr('cy');
 
   var separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2));
-  if(separation < radiusSum){
+  if(separation <= radiusSum){
     onCollision();
   }  
 };
 var ourTween = function(endData) {
   var enemy = d3.select(this);
-  var startPosition = [];
+  /*var startPosition = [];
   startPosition[0] = parseFloat(enemy.attr('cx'));
   startPosition[1] = parseFloat(enemy.attr('cy'));
-  var endPosition = [endData.x, endData.y];
+  var endPosition = [endData.x, endData.y];*/
     return function (tick){
       checkCollision(enemy);
-      var enemyNextPosition = [];
-      enemyNextPosition[0] = startPosition[0] + (endPosition[0] - startPosition[0]) * tick;
-      enemyNextPosition[1] = startPosition[1] + (endPosition[1] - startPosition[1]) * tick;
-      enemy.attr('cx', enemyNextPosition[0])
-           .attr('cy', enemyNextPosition[1]);
+      // var enemyNextPosition = [];
+      // enemyNextPosition[0] = startPosition[0] + (endPosition[0] - startPosition[0]) * tick;
+      // enemyNextPosition[1] = startPosition[1] + (endPosition[1] - startPosition[1]) * tick;
+      // enemy.attr('cx', enemyNextPosition[0])
+      //      .attr('cy', enemyNextPosition[1]);
     };
 };
 
